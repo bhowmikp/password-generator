187d655713358605d66bfc8953656310d13de861
bhowmikp <prantarbhowmik@yahoo.com>
2017-02-14 14:53:49 -0500

    Refactored code

--- password_generator.py
+++ password_generator.py
@@ -1,52 +1,175 @@
+#!/usr/bin/env python3
+
 import os
 
-PASSWORD_LENGTH = 30
 FILE_NAME = "Notes.txt"
 
 
-def generate_password():
-    ''' () -> str
+class ValidationError(Exception):
+    """Error raised if the user does not pick any type of characters"""
+    pass
 
-    Generate a randomly generated string using alphanumeric
-    characters and symbols
 
-    >>> generate_password()
-    '''
+class LengthError(Exception):
+    """Error raised if the length of string is too short or too long"""
+    pass
+
+
+def password_length_checker(length):
+    """Checks length of password asked for validity
+
+    Determines if the password is the correct length.
+    Less than 6 characters is too short. Greater than 40
+    characters is too long.
+
+    Args:
+        length: int determing the number of characters the
+            password should contain
+
+    Raises:
+        LengthError: Error occurred because password length is
+            invalid
+    """
+    if length < 6:
+        raise LengthError("WARNING: Password too short")
+    elif length > 40:
+        raise LengthError("WARNING: Password too long")
+
+
+def find_range(upper_case, lower_case, numbers, symbols):
+    """Finds the range of characters that the user wants
+
+    Finds the ascii representation of all the characters
+    that the user wants to create their password out of.
+
+    Args:
+        upper_case: boolean used to determine if upper
+            case characters are wanted
+        lower_case: boolean used to determine if lower
+            case characters are wanted
+        numbers: boolean used to determine if numbers
+            are wanted
+        symbols: boolean used to determine if symbols
+            are wanted
+
+    Returns:
+        list of ints containing ascii representation of
+        characters that is wanted
+
+    Raise:
+        ValidationError: An error occured where no type
+            of character selected to make the string
+    """
+    # sets the ascii range for uppercase letters
+    if upper_case:
+        upper_case_range = range(65, 90 + 1)
+    else:
+        upper_case_range = range(0, 0)
+
+    # sets the ascii range for lowercase letters
+    if lower_case:
+        lower_case_range = range(97, 122 + 1)
+    else:
+        lower_case_range = range(0, 0)
+
+    # sets the ascii range for numbers
+    if numbers:
+        numbers_range = range(48, 57 + 1)
+    else:
+        numbers_range = range(0, 0)
+
+    # sets the ascii range for symbols
+    if symbols:
+        symbols_range = list(range(33, 47 + 1)) + list(range(58, 64 + 1)) + \
+            list(range(91, 96 + 1)) + list(range(123, 126 + 1))
+    else:
+        symbols_range = range(0, 0)
+
+    # finds the range of letters accepted by the user
+    total_range = list(upper_case_range) + list(lower_case_range) + \
+        list(numbers_range) + list(symbols_range)
+
+    # if no range selected by user
+    if list(total_range) == []:
+        raise ValidationError("No character type has been selected")
+
+    return total_range
+
+
+def generate_password(length=30, upper_case=True,
+                      lower_case=True, numbers=True, symbols=True):
+    """Generates a randomized string.
+
+    Generates a string of specifications set in the paraments,
+    determining length and the type of characters acceptable.
+
+    Args:
+        length: Optional integer parameter. Set as 30 by default.
+            Determines the number of characters the password should contain
+        upper_case: Optional boolean parameter. Set as true by default.
+            Determines if upper case characters are wanted in the password
+        lower_case: Optional boolean parameter. Set as true by default.
+            Determines if lower case characters are wanted in the password.
+        numbers: Optional boolean parameter. Set as true by default.
+            Determines if numbers are wanted in the password.
+        symbols: Optional boolean parameter. Set as true by default.
+            Determines if symbols (i.e - ?, $ etc.) are wanted in the password.
+    """
     counter = 0
     password = ""
 
-    # the length of the password decided on
-    while (counter != PASSWORD_LENGTH):
-        # gets os to generate rand numbers for more randomized generation
-        x = (ord(os.urandom(1)))
+    # finds the range of letters that the user has chosen
+    try:
+        total_range = find_range(upper_case, lower_case, numbers, symbols)
+    except ValidationError:
+        return "Please fix settings. Password could not be generated"
 
-        # sets the range of ascii values that is acceptable for the password
-        if (x >= 33 and x <= 126):
-            password += chr(x)
+    # determine if the users password length is valid
+    try:
+        password_length_checker(length)
+    except LengthError:
+        return 'Password length invalid. Password must be between' \
+               '6 and 40 characters'
+
+    while counter < length:
+        # gets os to generate rand numbers for more randomized generation
+        letter = ord(os.urandom(1))
+
+        # only accepts values that the user has chosen
+        if letter in total_range:
+            password += chr(letter)
             counter += 1
 
     return password
 
 
-def create_file(fileName, site, user_name, password):
-    ''' (str, str, str, str) -> ()
+def create_file(file_name, site, user_name, password):
+    """Creates a file info containing site name, user name and password
 
-    Creates/appends file with the data provided
-
-    >>> create_file("File.txt", "Website", "Username", "Password")
-    '''
+    Args:
+        file_name: String containing the name of the file that will hold
+            the information
+        site: String containing the name of the site the username and
+            password is for
+        user_name: the user name for the site
+        password: the password for the site
+    """
     # create file with the info
-    file = open(fileName, 'a')
+    file = open(file_name, 'a')
     file.write(
         site + "\nUsername: " + user_name + "\nPassowrd: " + password + "\n\n")
     file.close()
 
 
-if __name__ == "__main__":
+def main():
     # get user info
     site = input("Enter the account password to be generated for: ")
     user_name = input("Enter your username: ")
-    # get passwor
+    # get password
     password = generate_password()
     # store user info in a file
     create_file(FILE_NAME, site, user_name, password)
+
+
+if __name__ == "__main__":
+    main()
