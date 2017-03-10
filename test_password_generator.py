46aa06a3c54f7f8c63a4d793d75b61ea8498aed8
bhowmikp <prantarbhowmik@yahoo.com>
2017-03-10 07:32:33 -0500

    Basic testing

--- test_password_generator.py
+++ test_password_generator.py
@@ -31,26 +31,84 @@ class TestPasswordLengthChecker(unittest.TestCase):
 
 class TestFindRange(unittest.TestCase):
 
+    def setUp(self):
+        self.uppercase = list(range(65, 90 + 1))
+        self.lowercase = list(range(97, 122 + 1))
+        self.numbers = list(range(48, 57 + 1))
+        self.symbols = list(range(33, 47 + 1)) + list(range(58, 64 + 1)) + list(range(91, 96 + 1)) + list(range(123, 126 + 1))
+
     def test_none(self):
         with self.assertRaises(ValidationError):
             find_range(False, False, False, False)
 
     def test_upper_case(self):
-        ascii_range = list(range(65, 90 + 1))
-        self.assertEquals(find_range(True, False, False, False), ascii_range, "Range of uppercase is 65 - 90. Return value not correct")
+        self.assertEquals(find_range(True, False, False, False), self.uppercase, "Range of uppercase is 65 - 90. Return value not correct")
 
     def test_lower_case(self):
-        ascii_range = list(range(97, 122 + 1))
-        self.assertEquals(find_range(False, True, False, False), ascii_range, "Range of lowercase is 97 - 122. Return value not correct")
+        self.assertEquals(find_range(False, True, False, False), self.lowercase, "Range of lowercase is 97 - 122. Return value not correct")
 
     def test_numbers(self):
-        ascii_range = list(range(48, 57 + 1))
-        self.assertEquals(find_range(False, False, True, False), ascii_range, "Range of symbols is 48 - 57. Return value not correct")
+        self.assertEquals(find_range(False, False, True, False), self.numbers, "Range of symbols is 48 - 57. Return value not correct")
 
     def test_symbols(self):
-        ascii_range = list(range(33, 47 + 1)) + list(range(58, 64 + 1)) + list(range(91, 96 + 1)) + list(range(123, 126 + 1))
-        self.assertEquals(find_range(False, False, False, True), ascii_range, "Not correct range of symbols")
+        self.assertEquals(find_range(False, False, False, True), self.symbols, "Not correct range of symbols")
 
+    def test_uppercase_and_lowercase(self):
+        ascii_range = list(self.uppercase) + list(self.lowercase)
+        self.assertEquals(find_range(True, True, False, False), ascii_range, "Not correct range")
+
+    def test_lowercase_and_symbols(self):
+        ascii_range = list(self.lowercase) + list(self.symbols)
+        self.assertEquals(find_range(False, True, False, True), ascii_range, "Not correct range")
+
+    def test_uppercase_and_numbers(self):
+        ascii_range = list(self.uppercase) + list(self.numbers)
+        self.assertEquals(find_range(True, False, True, False), ascii_range, "Not correct range")
+
+    def test_uppercase_numbers_symbols(self):
+        ascii_range = list(self.uppercase) + list(self.numbers) + list(self.symbols)
+        self.assertEquals(find_range(True, False, True, True), ascii_range, "Not correct range")
+
+    def test_lowercase_numbers_symbols(self):
+        ascii_range = list(self.lowercase) + list(self.numbers) + list(self.symbols)
+        self.assertEquals(find_range(False, True, True, True), ascii_range, "Not correct range")
+
+    def test_uppercase_lowercase_symbols(self):
+        ascii_range = list(self.uppercase) + list(self.lowercase) + list(self.symbols)
+        self.assertEquals(find_range(True, True, False, True), ascii_range, "Not correct range")
+
+    def test_all(self):
+        ascii_range = list(self.uppercase) + list(self.lowercase) + list(self.numbers) + list(self.symbols)
+        self.assertEquals(find_range(True, True, True, True), ascii_range, "Not correct range")
+
+
+class TestGeneratePassword(unittest.TestCase):
+    def setUp(self):
+        self.uppercase = list(range(65, 90 + 1))
+        self.lowercase = list(range(97, 122 + 1))
+        self.numbers = list(range(48, 57 + 1))
+        self.symbols = list(range(33, 47 + 1)) + list(range(58, 64 + 1)) + list(range(91, 96 + 1)) + list(range(123, 126 + 1))
+
+    def test_default(self):
+        status = True
+        length = 30
+
+        password = generate_password()
+        password_list = generate_password().split()
+
+        ascii_range = self.uppercase + self.lowercase + self.numbers + self.symbols
+
+        if (len(password) != length):
+            status = False
+
+        if len(password_list) != 1:
+            status = False
+
+        for letter in password:
+            if ord(letter) not in ascii_range:
+                status = False
+
+        self.assertTrue(status, "Password length or characters of password incorrect")
 
 
 
