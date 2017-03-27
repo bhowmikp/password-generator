fda9dd405306f38f2a46a21721422029c731d8ef
bhowmikp <prantarbhowmik@yahoo.com>
2017-03-27 11:50:44 -0400

    More documentation

--- password_generator_gui.py
+++ password_generator_gui.py
@@ -1,126 +1,174 @@
+'''Makes gui for password generator'''
+
 try:
     import password_generator as generator
-    import_correctly = True
-except:
-    import_correctly = False
-    
-from tkinter import *
+    IMPORT_CORRECTLY = True
+except BaseException:
+    IMPORT_CORRECTLY = False
+
+from tkinter import Tk, Frame, StringVar, IntVar, Label
+from tkinter import Entry, Checkbutton, Button, Menu, TclError
 
 
 class Application(Frame):
+    '''Makes the gui interface'''
+
     def __init__(self, master):
+        '''Calls necessary functions to create the app
+
+        Args:
+            master = Tk()
+        '''
         self.master = master
-        
-        Frame.__init__(self, master, highlightbackground="green", highlightthickness=1)
+
+        Frame.__init__(self, master, highlightbackground="green",
+                       highlightthickness=1)
         self.site = StringVar()
         self.username = StringVar()
         self.length = IntVar()
-        
+
         self.upper_case_status = IntVar()
         self.lower_case_status = IntVar()
         self.number_status = IntVar()
         self.symbol_status = IntVar()
-        
+
         self.message = StringVar()
         self.clipboard_message = StringVar()
-        
-        self.create_widgets()        
-    
+
+        self.create_widgets()
+
     def calculate(self):
+        '''Gets all the input from the user and generates password
+
+        Gets all input from the user in the gui and calls the function
+        to generate password
+        '''
         self.clipboard_message.set("")
-        
-        if (import_correctly == False):
+
+        if IMPORT_CORRECTLY is False:
             self.message.set("Code not found")
-        elif(self.site.get() == ""):
+        elif self.site.get() == "":
             self.message.set("Please enter name of site")
-        elif (self.username.get() == ""):
+        elif self.username.get() == "":
             self.message.set("Please enter username")
         else:
             try:
-                password = generator.generate_password(self.length.get(), self.upper_case_status.get(), self.lower_case_status.get(), self.number_status.get(), self.symbol_status.get())
-                               
+                # get all input from gui
+                password = generator.generate_password(
+                    self.length.get(),
+                    self.upper_case_status.get(),
+                    self.lower_case_status.get(),
+                    self.number_status.get(),
+                    self.symbol_status.get())
+
                 self.message.set("Password: " + password)
-                if (len(password) == self.length.get()):
+                if len(password) == self.length.get():
                     self.master.clipboard_clear()
                     self.master.clipboard_append(password)
-                    self.clipboard_message.set("Password copied to clipboard")   
-                    
-                    generator.create_file(self.site.get(), self.username.get(), password)
-                    
+                    self.clipboard_message.set("Password copied to clipboard")
+
+                    generator.create_file(
+                        self.site.get(), self.username.get(), password)
+
             except TclError:
                 self.message.set("Please enter valid length")
-    
+
     def create_widgets(self):
+        '''Creates all the elements inside the frame'''
         row_location = 0
-        
+
         # welcome message
-        Label(text='Generates password for account', bg='#2A2C2B', fg="white").grid(row=row_location, column=1, columnspan=2, padx=125, pady= 10)
-        row_location+= 1
-        
+        Label(
+            text='Generates password for account',
+            bg='#2A2C2B', fg="white").grid(
+                row=row_location, column=1, columnspan=2, padx=125, pady=10)
+        row_location += 1
+
         # get account name
-        Label(text="Account For: ", bg='#2A2C2B', fg="white").grid(row=row_location, column=1, sticky = "E", pady = 10)
-        Entry(textvariable = self.site).grid(sticky = "W", row=row_location, column=2)
-        row_location+= 1
-        
+        Label(text="Account For: ", bg='#2A2C2B', fg="white").grid(
+            row=row_location, column=1, sticky="E", pady=10)
+        Entry(textvariable=self.site).grid(
+            sticky="W", row=row_location, column=2)
+        row_location += 1
+
         # get username
-        Label(text="Username: ", bg='#2A2C2B', fg="white").grid(row=row_location, column=1, sticky = "E", pady = 10)
-        Entry(textvariable = self.username).grid(sticky = "W", row=row_location, column=2)
-        row_location+= 1
-        
+        Label(text="Username: ", bg='#2A2C2B', fg="white").grid(
+            row=row_location, column=1, sticky="E", pady=10)
+        Entry(textvariable=self.username).grid(
+            sticky="W", row=row_location, column=2)
+        row_location += 1
+
         # get length
-        Label(text="Password Length: ", bg='#2A2C2B', fg="white").grid(row=row_location, column=1, sticky = "E", pady = 10)
-        Entry(textvariable = self.length).grid(sticky = "W", row=row_location, column=2)        
-        row_location+= 1
-        
+        Label(text="Password Length: ", bg='#2A2C2B', fg="white").grid(
+            row=row_location, column=1, sticky="E", pady=10)
+        Entry(textvariable=self.length).grid(
+            sticky="W", row=row_location, column=2)
+        row_location += 1
+
         # checkboxes
-        Checkbutton(bg='#2A2C2B', variable=self.upper_case_status).grid(sticky = "E", row=row_location, column=1 , pady = 10)
-        Label(text="Uppercase Character [A-Z]", bg='#2A2C2B', fg="white").grid(sticky = "W", row=row_location, column=2)
-        row_location+= 1
-        
-        Checkbutton(bg='#2A2C2B', variable=self.lower_case_status).grid(sticky = "E", row=row_location, column=1, pady = 10)
-        Label(text="Lowercase Character [a-z]", bg='#2A2C2B', fg="white").grid(sticky = "W", row=row_location, column=2)        
-        row_location+= 1
-        
-        Checkbutton(bg='#2A2C2B', variable=self.number_status).grid(sticky = "E", row=row_location, column=1, pady = 10)
-        Label(text="Numbers [0-9]", bg='#2A2C2B', fg="white").grid(sticky = "W", row=row_location, column=2)           
-        row_location+= 1
-        
-        Checkbutton(bg='#2A2C2B', variable=self.symbol_status).grid(sticky = "E", row=row_location, column=1, pady = 10)
-        Label(text="Symbols [Ex - ?, $]", bg='#2A2C2B', fg="white").grid(sticky = "W", row=row_location, column=2)           
-        row_location+= 1
-        
+        Checkbutton(bg='#2A2C2B', variable=self.upper_case_status).grid(
+            sticky="E", row=row_location, column=1, pady=10)
+        Label(text="Uppercase Character [A-Z]", bg='#2A2C2B',
+              fg="white").grid(sticky="W", row=row_location, column=2)
+        row_location += 1
+
+        Checkbutton(bg='#2A2C2B', variable=self.lower_case_status).grid(
+            sticky="E", row=row_location, column=1, pady=10)
+        Label(text="Lowercase Character [a-z]", bg='#2A2C2B',
+              fg="white").grid(sticky="W", row=row_location, column=2)
+        row_location += 1
+
+        Checkbutton(bg='#2A2C2B', variable=self.number_status).grid(
+            sticky="E", row=row_location, column=1, pady=10)
+        Label(text="Numbers [0-9]", bg='#2A2C2B',
+              fg="white").grid(sticky="W", row=row_location, column=2)
+        row_location += 1
+
+        Checkbutton(bg='#2A2C2B', variable=self.symbol_status).grid(
+            sticky="E", row=row_location, column=1, pady=10)
+        Label(text="Symbols [Ex - ?, $]", bg='#2A2C2B',
+              fg="white").grid(sticky="W", row=row_location, column=2)
+        row_location += 1
+
         self.upper_case_status.set(True)
         self.lower_case_status.set(True)
         self.number_status.set(True)
         self.symbol_status.set(True)
         self.length.set(30)
-        
+
         # generate password
-        Button(text = "Generate Password", command = self.calculate).grid(row=row_location, column = 1, columnspan = 2, pady = 20)
-        row_location+= 1
-        
+        Button(text="Generate Password", command=self.calculate).grid(
+            row=row_location, column=1, columnspan=2, pady=20)
+        row_location += 1
+
         # display message
-        Label(textvariable=self.message, bg='#2A2C2B', fg="white").grid(sticky = "W", row=row_location, column=1, padx = 125, columnspan = 2)
-        row_location+= 1
-        
+        Label(textvariable=self.message, bg='#2A2C2B', fg="white").grid(
+            sticky="W", row=row_location, column=1, padx=125, columnspan=2)
+        row_location += 1
+
         # inform about clipboard
-        Label(textvariable=self.clipboard_message, bg='#2A2C2B', fg="white").grid(sticky = "W", row=row_location, column=1, padx = 125, columnspan = 2)        
-        
-    
+        Label(
+            textvariable=self.clipboard_message,
+            bg='#2A2C2B',
+            fg="white").grid(sticky="W", row=row_location, column=1, padx=125, columnspan=2)
+
+
 def main():
+    '''Makes the application
+    '''
     root = Tk()
     app = Application(master=root)
     app.master.title("Password Generator")
     root.minsize(500, 500)
-    root.maxsize(600, 600)    
-    
+    root.maxsize(600, 600)
 
     menubar = Menu(root)
     filemenu = Menu(menubar, tearoff=0)
     filemenu.add_command(label="Exit", command=root.destroy)
-    menubar.add_cascade(label="File", menu=filemenu)    
+    menubar.add_cascade(label="File", menu=filemenu)
     root.config(bg='#2A2C2B', menu=menubar)
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
--- test_password_generator.py
+++ test_password_generator.py
@@ -1,96 +1,222 @@
+'''Unit test for password generator'''
+
 import unittest
 import os
-from password_generator import password_length_checker, LengthError, find_range, ValidationError, generate_password, create_file
+from password_generator import password_length_checker, LengthError
+from password_generator import find_range, ValidationError
+from password_generator import generate_password, create_file
+
 
 class TestPasswordLengthChecker(unittest.TestCase):
+    '''Test password_length_checker'''
 
     def test_too_short_negative(self):
+        '''Test if the param is a negative number'''
         with self.assertRaises(LengthError):
             password_length_checker(-1)
 
     def test_too_short_positive(self):
+        '''Test if password is positive but below
+        acceptable range'''
         with self.assertRaises(LengthError):
             password_length_checker(5)
 
     def test_no_errors_1(self):
-        self.assertEqual(password_length_checker(6), None, "Returns something that's not None")
+        '''Test with correct param'''
+        self.assertEqual(password_length_checker(6), None,
+                         "Returns something that's not None")
 
     def test_no_errors_2(self):
-        self.assertEqual(password_length_checker(15), None, "Returns something that's not None")
+        '''Test with correct param'''
+        self.assertEqual(password_length_checker(15), None,
+                         "Returns something that's not None")
 
     def test_no_errors_3(self):
-        self.assertEqual(password_length_checker(40), None, "Returns something that's not None")
+        '''Test with correct param'''
+        self.assertEqual(password_length_checker(40), None,
+                         "Returns something that's not None")
 
     def test_too_long_1(self):
+        '''Test with number over accepted range'''
         with self.assertRaises(LengthError):
             password_length_checker(41)
 
     def test_too_long_2(self):
+        '''Test with number over accepted range'''
         with self.assertRaises(LengthError):
             password_length_checker(1000)
 
 
 class TestFindRange(unittest.TestCase):
+    '''Test find_range'''
 
     def setUp(self):
+        '''Sets up the variables used to test the function'''
         self.uppercase = list(range(65, 90 + 1))
         self.lowercase = list(range(97, 122 + 1))
         self.numbers = list(range(48, 57 + 1))
-        self.symbols = list(range(33, 47 + 1)) + list(range(58, 64 + 1)) + list(range(91, 96 + 1)) + list(range(123, 126 + 1))
+        self.symbols = list(range(33, 47 + 1)) + \
+                       list(range(58, 64 + 1)) + \
+                       list(range(91, 96 + 1)) + \
+                       list(range(123, 126 + 1))
 
     def test_none(self):
+        '''Test with all types of characters set as False'''
         with self.assertRaises(ValidationError):
             find_range(False, False, False, False)
 
     def test_upper_case(self):
-        self.assertEqual(find_range(True, False, False, False), self.uppercase, "Range of uppercase is 65 - 90. Return value not correct")
+        '''Test with only uppercase characters'''
+        self.assertEqual(
+            find_range(
+                True,
+                False,
+                False,
+                False),
+            self.uppercase,
+            "Range of uppercase is 65 - 90. Return value not correct")
 
     def test_lower_case(self):
-        self.assertEqual(find_range(False, True, False, False), self.lowercase, "Range of lowercase is 97 - 122. Return value not correct")
+        '''Test with only lowercase characters'''
+        self.assertEqual(
+            find_range(
+                False,
+                True,
+                False,
+                False),
+            self.lowercase,
+            "Range of lowercase is 97 - 122. Return value not correct")
 
     def test_numbers(self):
-        self.assertEqual(find_range(False, False, True, False), self.numbers, "Range of symbols is 48 - 57. Return value not correct")
+        '''Tets with only numbers'''
+        self.assertEqual(
+            find_range(
+                False,
+                False,
+                True,
+                False),
+            self.numbers,
+            "Range of symbols is 48 - 57. Return value not correct")
 
     def test_symbols(self):
-        self.assertEqual(find_range(False, False, False, True), self.symbols, "Not correct range of symbols")
+        '''Test with only symbols'''
+        self.assertEqual(
+            find_range(
+                False,
+                False,
+                False,
+                True),
+            self.symbols,
+            "Not correct range of symbols")
 
     def test_uppercase_and_lowercase(self):
+        '''Test with uppercase and lowercase characters'''
         ascii_range = list(self.uppercase) + list(self.lowercase)
-        self.assertEqual(find_range(True, True, False, False), ascii_range, "Not correct range")
+        self.assertEqual(
+            find_range(
+                True,
+                True,
+                False,
+                False),
+            ascii_range,
+            "Not correct range")
 
     def test_lowercase_and_symbols(self):
+        '''Test with lowercase characters and symbols'''
         ascii_range = list(self.lowercase) + list(self.symbols)
-        self.assertEqual(find_range(False, True, False, True), ascii_range, "Not correct range")
+        self.assertEqual(
+            find_range(
+                False,
+                True,
+                False,
+                True),
+            ascii_range,
+            "Not correct range")
 
     def test_uppercase_and_numbers(self):
+        '''Test with uppercase letters and numebrs'''
         ascii_range = list(self.uppercase) + list(self.numbers)
-        self.assertEqual(find_range(True, False, True, False), ascii_range, "Not correct range")
+        self.assertEqual(
+            find_range(
+                True,
+                False,
+                True,
+                False),
+            ascii_range,
+            "Not correct range")
 
     def test_uppercase_numbers_symbols(self):
-        ascii_range = list(self.uppercase) + list(self.numbers) + list(self.symbols)
-        self.assertEqual(find_range(True, False, True, True), ascii_range, "Not correct range")
+        '''Test with uppercase characters, numbers and symbols'''
+        ascii_range = list(self.uppercase) + \
+            list(self.numbers) + list(self.symbols)
+        self.assertEqual(
+            find_range(
+                True,
+                False,
+                True,
+                True),
+            ascii_range,
+            "Not correct range")
 
     def test_lowercase_numbers_symbols(self):
-        ascii_range = list(self.lowercase) + list(self.numbers) + list(self.symbols)
-        self.assertEqual(find_range(False, True, True, True), ascii_range, "Not correct range")
+        '''Test with lowercase characters, numbers and symbols'''
+        ascii_range = list(self.lowercase) + \
+            list(self.numbers) + list(self.symbols)
+        self.assertEqual(
+            find_range(
+                False,
+                True,
+                True,
+                True),
+            ascii_range,
+            "Not correct range")
 
-    def test_uppercase_lowercase_symbols(self):
-        ascii_range = list(self.uppercase) + list(self.lowercase) + list(self.symbols)
-        self.assertEqual(find_range(True, True, False, True), ascii_range, "Not correct range")
+    def test_upper_lowercase_symbols(self):
+        '''Test with uppercase and lowercase characters and symbols'''
+        ascii_range = list(self.uppercase) + \
+            list(self.lowercase) + list(self.symbols)
+        self.assertEqual(
+            find_range(
+                True,
+                True,
+                False,
+                True),
+            ascii_range,
+            "Not correct range")
 
     def test_all(self):
-        ascii_range = list(self.uppercase) + list(self.lowercase) + list(self.numbers) + list(self.symbols)
-        self.assertEqual(find_range(True, True, True, True), ascii_range, "Not correct range")
+        '''Test with uppercase and lowercase characters, and symbols
+        and numbers
+        '''
+        ascii_range = list(self.uppercase) + list(self.lowercase) + \
+            list(self.numbers) + list(self.symbols)
+        self.assertEqual(
+            find_range(
+                True,
+                True,
+                True,
+                True),
+            ascii_range,
+            "Not correct range")
 
 
 class TestGeneratePassword(unittest.TestCase):
+    '''Test generate_password'''
+
     def setUp(self):
+        '''Sets up the variables used to test the function'''
         self.uppercase = list(range(65, 90 + 1))
         self.lowercase = list(range(97, 122 + 1))
         self.numbers = list(range(48, 57 + 1))
-        self.symbols = list(range(33, 47 + 1)) + list(range(58, 64 + 1)) + list(range(91, 96 + 1)) + list(range(123, 126 + 1))
+        self.symbols = list(range(33, 47 + 1)) + \
+                       list(range(58, 64 + 1)) + \
+                       list(range(91, 96 + 1)) + \
+                       list(range(123, 126 + 1))
 
     def test_default(self):
+        '''Check if length of password is 30 by default and all character
+        types are used by default to generate password
+        '''
         status = True
         length = 30
 
@@ -109,58 +235,85 @@ class TestGeneratePassword(unittest.TestCase):
             if ord(letter) not in ascii_range:
                 status = False
 
-        self.assertTrue(status, "Password length or characters of password incorrect")
+        self.assertTrue(
+            status,
+            "Password length or characters of password incorrect")
+
 
 class TestCreateFile(unittest.TestCase):
+    '''Test create_file'''
+
     def test_file_creation_default(self):
+        '''Test file with one case of input and default params'''
         create_file("Site", "Name", "Hello123", "Nothing.txt")
 
         with open('Nothing.txt') as file:
             lines = file.readlines()
 
         self.assertEqual(lines[0].strip(), 'Site', 'Site name not correct')
-        self.assertEqual(lines[1].strip(), 'Username: Name', 'Username not correct')
-        self.assertEqual(lines[2].strip(), 'Password: Hello123', 'Password not correct')
+        self.assertEqual(
+            lines[1].strip(),
+            'Username: Name',
+            'Username not correct')
+        self.assertEqual(
+            lines[2].strip(),
+            'Password: Hello123',
+            'Password not correct')
         self.assertEqual(lines[3], '\n', 'No space after input')
 
         os.remove("Nothing.txt")
 
     def test_file_creation(self):
+        '''Test file creation with different inputs'''
         create_file("None", "No Name", "Nothing")
 
         with open('Notes.txt') as file:
             lines = file.readlines()
 
         self.assertEqual(lines[0].strip(), 'None', 'Site name not correct')
-        self.assertEqual(lines[1].strip(), 'Username: No Name', 'Username not correct')
-        self.assertEqual(lines[2].strip(), 'Password: Nothing', 'Password not correct')
+        self.assertEqual(
+            lines[1].strip(),
+            'Username: No Name',
+            'Username not correct')
+        self.assertEqual(
+            lines[2].strip(),
+            'Password: Nothing',
+            'Password not correct')
         self.assertEqual(lines[3], '\n', 'No space after input')
 
         os.remove("Notes.txt")
 
-
     def test_file_two_site(self):
+        '''Test with two sites being created'''
         create_file("Site", "Name", "Hello123", "Nothing.txt")
-        create_file("None", "No Name", "Nothing")
+        create_file("None", "No Name", "Nothing", "Nothing.txt")
 
         with open('Nothing.txt') as file:
             lines = file.readlines()
 
-        with open('Notes.txt') as file1:
-            lines1 = file1.readlines()
-
         self.assertEqual(lines[0].strip(), 'Site', 'Site name not correct')
-        self.assertEqual(lines[1].strip(), 'Username: Name', 'Username not correct')
-        self.assertEqual(lines[2].strip(), 'Password: Hello123', 'Password not correct')
+        self.assertEqual(
+            lines[1].strip(),
+            'Username: Name',
+            'Username not correct')
+        self.assertEqual(
+            lines[2].strip(),
+            'Password: Hello123',
+            'Password not correct')
         self.assertEqual(lines[3], '\n', 'No space after input')
 
-        self.assertEqual(lines1[0].strip(), 'None', 'Site name not correct')
-        self.assertEqual(lines1[1].strip(), 'Username: No Name', 'Username not correct')
-        self.assertEqual(lines1[2].strip(), 'Password: Nothing', 'Password not correct')
-        self.assertEqual(lines1[3], '\n', 'No space after input')
+        self.assertEqual(lines[4].strip(), 'None', 'Site name not correct')
+        self.assertEqual(
+            lines[5].strip(),
+            'Username: No Name',
+            'Username not correct')
+        self.assertEqual(
+            lines[6].strip(),
+            'Password: Nothing',
+            'Password not correct')
+        self.assertEqual(lines[7], '\n', 'No space after input')
 
         os.remove('Nothing.txt')
-        os.remove('Notes.txt')
 
 if __name__ == "__main__":
     unittest.main()
